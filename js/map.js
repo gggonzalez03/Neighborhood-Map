cd38d74b667c968829e947d4c9e20e118ff7c591
ginggonzalez03 <ginggonzalez3@gmail.com>
2017-01-21 23:50:06 -0800

    refactor: Optimized map.js
    Created functions for the processes that are repeatedly done throughout the js
    file. Comments are already included.

--- js/map.js
+++ js/map.js
@@ -1,29 +1,47 @@
+// Takes an array of Marker objects
+function setMapMarkers(map, markers) {
+    for (var i = 0; i < markers.length; i++) {
+    	markers[i].setMap(map);
+    }
+}
+
+function clearMapMarkers(markers){
+	setMapMarkers(null, markers);
+}
+
+// Takes an array of coordinates and
+// returns an array of Marker objects
+function makeMapMarkers(coordinates){
+
+	var markers = [];
+
+	for(var i = 0; i < coordinates.length; i++){
+		markers.push(new google.maps.Marker({
+			position: coordinates[i]
+		}));
+	}
+
+	return markers;
+}
+
 // Initialize Map
 var initMap = function() {
 	// Map configuration
 	var mapSetup = {
-		center: new google.maps.LatLng(model.favoritePlaces[0]),
+		center: new google.maps.LatLng(model.favoritePlaces[4]),
 		zoom: 11,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 
 	//Show the map in the element with id of "map"
-	var map = new google.maps.Map(document.getElementById('map'), mapSetup);
+	// and store the map object in variable map
+	var map = new google.maps.Map($('#map')[0], mapSetup);
 
 	// Place the initial markers
-	var marker = [];
-	for(var i = 0; i < model.favoritePlaces.length; i++){
-		marker.push(new google.maps.Marker({
-			position: model.favoritePlaces[i]
-		}));
-		marker[i].setMap(map);
-	}
+	var markers = makeMapMarkers(model.favoritePlaces);
 
-	function setMapOnAll(map) {
-        for (var i = 0; i < marker.length; i++) {
-        	marker[i].setMap(map);
-        }
-    }
+	// Place markers
+	setMapMarkers(map, markers);
 
     // This will make the input a searchBox
 	var searchBox = new google.maps.places.SearchBox($('#searchBox')[0]);
@@ -35,17 +53,26 @@ var initMap = function() {
 
 	// Change the markers whenever a search is done
 	searchBox.addListener('places_changed', function() {
-		setMapOnAll(null);
-  		marker = [];
+
+		// Clear markers on the map
+		clearMapMarkers(markers);
+
+		// Delete all markers stored
+  		markers = [];
+
+  		// This will store the coordinates received from google
+  		coordinates = [];
+
 		for(var i = 0; i < searchBox.getPlaces().length; i++){
-			marker.push(new google.maps.Marker({
-				position: {
+			coordinates.push({
 					lat: searchBox.getPlaces()[i].geometry.location.lat(),
 					lng: searchBox.getPlaces()[i].geometry.location.lng()
-				}
-			}));
-
-			marker[i].setMap(map);
+				});
 		}
+
+		// Make markers
+		markers = makeMapMarkers(coordinates);
+		// Place markers
+		setMapMarkers(map, markers);
 	});
 };
