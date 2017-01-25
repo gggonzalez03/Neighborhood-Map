f0edc06416bf04565c7c0cd62ff8af80f6dfaa83
ginggonzalez03 <ginggonzalez3@gmail.com>
2017-01-24 21:59:06 -0800

    chore: Added formatted_address
    Addresses are added to the pre-loaded places.

--- js/app.js
+++ js/app.js
@@ -2,26 +2,31 @@ var model = {
 	favoritePlaces: [
 		{
 			name: "San Jose",
+			formatted_address: "San Jose, CA, USA",
 			lat: 37.3382082,
 			lng: -122.0450548
 		},
 		{
 			name: "De Anza College",
+			formatted_address: "21250 Stevens Creek Boulevard, Cupertino, CA, United States",
 			lat: 37.31953960000001,
 			lng: -122.04447070000003
 		},
         {
         	name: "Cathedral of Faith",
+        	formatted_address: "2315 Canoas Garden Avenue, San Jose, CA, United States",
         	lat: 37.2920879,
         	lng: -121.87471370000003
         },
         {
         	name: "Children's Discovery Museum of San Jose",
+        	formatted_address: "180 Woz Way, San Jose, CA, United States",
         	lat: 37.32686159999999,
         	lng: -121.89209349999999
         },
         {
         	name: "Intel Museum",
+        	formatted_address: "2200 Mission College Boulevard, Santa Clara, CA, United States",
         	lat: 37.3875909,
         	lng: -121.9637869
         }
